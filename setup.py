b0b94af5775a9d73afadf305811991f754b0a6b6
Fernando dos Reis Santos Filho <ferfilho@motorola.com>
2023-05-30 15:57:03 -0300

    doc: Update directory structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A machine learning service for calculating the price and recommend real estate',
+    author='Fernando Filho',
+    license='MIT',
+)
